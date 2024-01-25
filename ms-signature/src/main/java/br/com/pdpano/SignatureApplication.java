7d17052843580113927aafe1d46cba359498ecc3
pdpan0 <lm.m4rtins@gmail.com>
2024-01-25 17:09:22 -0300

    feat: initial architecture

---
+++ ms-signature/src/main/java/br/com/pdpano/SignatureApplication.java
@@ -0,0 +1,7 @@
+package br.com.pdpano;
+
+public class SignatureApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ ms-user/src/main/java/br/com/pdpano/UserApplication.java
@@ -0,0 +1,7 @@
+package br.com.pdpano;
+
+public class UserApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
